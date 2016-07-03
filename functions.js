0e9d58b1ec986dcc3b5019f3ed5d519fff3be73a
Andrés David Liu Fong <idontcarethis@yahoo.com>
2016-07-02 19:29:55 -0500

    General bug fixing and fix for the testimonial section.
    The profile images are now the ones beign absolutely centered in its
    container instead of the actual copy.

--- functions.js
+++ functions.js
@@ -9,12 +9,21 @@ $(document).ready(function () {
     $("img.arrow").click(function () {
         $("section.tagline").ScrollTo();
     });
+    
+    var footer = $("footer");
+    var heart = $("footer span.heart");
+    
+    footer.hover(function () {
+        heart.css("color", "#c0392b");
+    }, function () {
+        heart.css("color", "inherit");
+    });
 
     var linksLive = ["http://www.andresliu.xyz/portfolio/websites/comicastic", "http://www.andresliu.xyz/portfolio/websites/cars", "http://www.andresliu.xyz/portfolio/websites/safari", "http://www.andresliu.xyz/portfolio/websites/community",
-    "http://www.andresliu.xyz/portfolio/websites/wedding", 
+    "http://www.andresliu.xyz/portfolio/websites/wedding",
     "http://www.andresliu.xyz/portfolio/websites/coffee",
     "http://www.andresliu.xyz/portfolio/websites/green"];
-    var linksCode = ["#", "#", "#", "#", "#", "#", "#"];
+    var linksCode = ["https://github.com/ADavidLiu/Comicastic", "https://github.com/ADavidLiu/Car-design", "https://github.com/ADavidLiu/Safari", "https://github.com/ADavidLiu/Community-united", "https://github.com/ADavidLiu/Wedding", "https://github.com/ADavidLiu/Gourmet-coffee", "https://github.com/ADavidLiu/Green-georgie"];
     var images = ["images/portfolio/websites/1-Comicastic.jpg", "images/portfolio/websites/2-Car-Design.jpg",
     "images/portfolio/websites/3-Safari-Reviver.jpg",
     "images/portfolio/websites/4-Community-United.jpg",
@@ -23,6 +32,38 @@ $(document).ready(function () {
     "images/portfolio/websites/7-GreenGeorgie.jpg"];
     var slider;
     var currentNumber = 1;
+
+    function changeLinks(index) {
+        $("#linkLive").attr("href", linksLive[index]);
+        $("#linkCode").attr("href", linksCode[index]);
+    }
+
+    function changeImage() {
+        $("img.current + img").fadeIn("fast").addClass("current");
+        $(".image-wrapper").children(".current").first().removeClass("current");
+    }
+
+    function loopSlider() {
+        $(".image-wrapper img.current").removeClass("current");
+        $(".image-wrapper img:first-child").addClass("current").fadeIn("fast");
+    }
+
+    function loopIndicators() {
+        $(".indicators .indicators-bullet.bullet-active").removeClass("bullet-active");
+        $(".indicators .indicators-bullet").first().addClass("bullet-active");
+    }
+
+    function changeIndicators() {
+        $(".indicators .indicators-bullet.bullet-active").parent().next().children().addClass("bullet-active");
+        $(".indicators .indicators-bullet.bullet-active").first().removeClass("bullet-active");
+    }
+
+    function resetSlider() {
+        loopSlider();
+        changeLinks(0);
+        loopIndicators();
+        currentNumber = 1;
+    }
 
     // For the portfolio modal
     $(".article-btn-more img:not('#btn-illustrations')").click(function () {
@@ -34,23 +75,36 @@ $(document).ready(function () {
         slider = setInterval(function () {
             $("img.current").fadeOut("fast", function () {
                 if ($("img.current").next().size() > 0) {
-                    $("img.current + img").fadeIn("fast").addClass("current");
-                    $(".image-wrapper").children(".current").first().removeClass("current");
-                    
-                    // Change the links
-                    $("#linkLive").attr("href", linksLive[currentNumber]);
-                    currentNumber++;
-                    
+
+                    changeImage();
+                    changeLinks(currentNumber);
+                    changeIndicators();
+
+                    if (currentNumber < 7) {
+                        currentNumber++;
+                    } else {
+                        currentNumber = 6;
+                    }
+
+
                 } else {
-                    $(".image-wrapper img.current").removeClass("current");
-                    $(".image-wrapper img:first-child").addClass("current").fadeIn("fast");
-                    
-                    // Change the links
-                    $("#linkLive").attr("href", linksLive[0]);
-                    currentNumber = 1;
+                    resetSlider();
                 }
             });
         }, 3000);
+
+        // Change image on indicator click
+        $(".indicators li").click(function () {
+            clearInterval(slider);
+            var numItem = $(this).parent().children().index(this);
+            $(".indicators .indicators-bullet.bullet-active").removeClass("bullet-active");
+            $(this).children().addClass("bullet-active");
+            $("img.current").fadeOut("fast").removeClass("current");
+            $(".image-wrapper img:nth-child(" + (numItem + 1) + ")").fadeIn("fast").addClass("current");
+            changeLinks(numItem);
+            currentNumber = numItem;
+        });
+
     });
 
     $(".portfolio-btn-close").click(function () {
@@ -58,6 +112,9 @@ $(document).ready(function () {
         setTimeout(function () {
             $(".portfolio").removeClass("portfolio-active");
         }, 100);
-        clearInterval(slider);
+        setTimeout(function () {
+            clearInterval(slider);
+            resetSlider();
+        }, 200);
     });
 });
